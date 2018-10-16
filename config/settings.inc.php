521d3b32b89dfdc45f70042cd1f30cb1f3742bc6
DESKTOP-J3R0FDH\lam tuyen <tuyenlqvnp@gmail.com>
2018-10-16 09:08:59 +0700

    no message

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -1,12 +1,12 @@
 <?php
 
-define('__PS_BASE_URI__', '/admin99/');
+define('__PS_BASE_URI__', '/');
 define('_THEME_NAME_', 'prestashop');
-define('_DB_NAME_', 'thaitruc_data');
-define('_DB_SERVER_', '127.0.0.1');
-define('_DB_USER_', 'root');
+define('_DB_NAME_', 'heroku_4f434fcc9d65e20');
+define('_DB_SERVER_', 'us-cdbr-iron-east-01.cleardb.net');
+define('_DB_USER_', 'b559f7eaed8029');
 define('_DB_PREFIX_', 'ps_');
-define('_DB_PASSWD_', '1234567t');
+define('_DB_PASSWD_', 'ef160591');
 define('_DB_TYPE_', 'MySQL');
 define('_COOKIE_KEY_', 'fos5PUM3Fe0TNgA47Ej16toEAB53amu6iLooDu4g86dbhyIOIaK3sDzQ');
 define('_COOKIE_IV_', 'r5VjWVSx');
